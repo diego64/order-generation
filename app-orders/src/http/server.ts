8d1c8ebab0e8c2b5bbd93364abdf84ef0f7151d8
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2025-09-02 17:41:16 -0300

    raw: configurando ferramentas de observabilidade (#5)

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,5 +1,9 @@
+import '@opentelemetry/auto-instrumentations-node/register'
+import { trace } from '@opentelemetry/api'
+
 import { fastify } from 'fastify'
 import { randomUUID } from 'node:crypto'
+import { setTimeout } from 'node:timers/promises'
 import { fastifyCors } from '@fastify/cors'
 import { z } from 'zod'
 import { 
@@ -7,10 +11,11 @@ import {
   validatorCompiler,
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
-import { channels } from '../broker/channels/index.ts'
+//import { channels } from '../broker/channels/index.ts'
 import { schema } from '../db/schema/index.ts'
 import { db } from '../db/client.ts'
 import { dispatchOrderCreated } from '../broker/messages/order-created.ts'
+import { tracer } from '../tracer/tracer.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -36,20 +41,30 @@ app.post('/orders', {
 
   const orderId = randomUUID()
 
-  dispatchOrderCreated({
-    orderId,
-    amount,
-    customer: {
-      id: 'c02adfba-bd51-414e-ba7e-caa94c36c257'
-    }
-  })
-
   await db.insert(schema.orders).values({
     id: orderId,
     //customerId: randomUUID(),
     customerId: 'c02adfba-bd51-414e-ba7e-caa94c36c257',
     amount,
   })
+
+  const span = tracer.startSpan('Verificar log pois existe suspeita de alto tempo de espera')
+
+  await setTimeout(2000)
+
+  span.end()
+
+  trace.getActiveSpan()?.setAttribute('order.id', orderId)
+
+  dispatchOrderCreated({
+    orderId,
+    amount,
+    customer: {
+      id: 'c02adfba-bd51-414e-ba7e-caa94c36c257'
+    },
+  })
+
+  return reply.status(201).send()
 })
 
 app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
