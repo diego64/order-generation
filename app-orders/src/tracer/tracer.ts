f2b21e9363bb2a845eae343d505f23fff32409ef
Diego F. Oliveira <diegoflg.oliveira@gmail.com>
2025-09-06 16:02:58 -0300

    hotfix/variaveis dockerfile (#19)
    * fix: env no dockerfile
    
    * fix: colocando a variavel para rodar no container
    
    * fix: acerto no arquivo dockerfile
    
    * fix: setando as variaveis ambiente
    
    * fix: dupla verificacao das variaveis
    
    ---------
    
    Co-authored-by: Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>

--- app-orders/src/tracer/tracer.ts
+++ app-orders/src/tracer/tracer.ts
@@ -1,9 +1,9 @@
-import { trace } from '@opentelemetry/api'
+import { trace } from '@opentelemetry/api';
 
 const OTEL_SERVICE_NAME = process.env.OTEL_SERVICE_NAME || "app-orders";
 
-if (!process.env.OTEL_SERVICE_NAME) {
+if (!OTEL_SERVICE_NAME) {
   throw new Error('OTEL_SERVICE_NAME must be configured.')
 }
 
-export const tracer = trace.getTracer(process.env.OTEL_SERVICE_NAME)
+export const tracer = trace.getTracer(OTEL_SERVICE_NAME);
